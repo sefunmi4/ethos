0b4f53ce75353441f7217607df19f3b6d8ca30b8
sefunmi4 <sefunmiashiru0@gmail.com>
2025-06-06 18:42:50 -0400

    fix backend types and routes

--- ethos-backend/src/models/stores.ts
+++ ethos-backend/src/models/stores.ts
@@ -1,10 +1,11 @@
 
 // src/models/GitModel.ts
 import type { DBSchema } from '../types/db';
-import { createDataStore } from '../utils/loaders'
+import { createDataStore } from '../utils/loaders';
 
 export const boardsStore = createDataStore<DBSchema['boards']>('boards.json');
 export const gitStore = createDataStore<DBSchema['git']>('git.json');
 export const postsStore = createDataStore<DBSchema['posts']>('posts.json');
 export const questsStore = createDataStore<DBSchema['quests']>('quests.json');
-export const usersStore = createDataStore<DBSchema['users']>('user.json');
+export const usersStore = createDataStore<DBSchema['users']>('users.json');
+export const reactionsStore = createDataStore<string[]>('reactions.json');
--- ethos-backend/src/types/express.ts
+++ ethos-backend/src/types/express.ts
@@ -1,8 +1,14 @@
-import { Request } from 'express';
-
-export interface AuthenticatedRequest extends Request {
+import { Request, ParamsDictionary } from 'express';
+import type { ParsedQs } from 'qs';
+export interface AuthenticatedRequest<
+  P = ParamsDictionary,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = ParsedQs
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
   user?: {
     id: string;
     role?: string;
+    username?: string;
   };
 }
