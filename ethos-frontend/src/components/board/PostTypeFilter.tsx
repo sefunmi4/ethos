0e49f37f137e33d68773f7313362fb4fac44fbd4
sefunmi4 <sefunmiashiru0@gmail.com>
2025-06-09 06:23:46 -0400

    Add post type filter on home page

---
+++ ethos-frontend/src/components/board/PostTypeFilter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Select from '../ui/Select';
+
+interface PostTypeFilterProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const options = [
+  { value: '', label: 'All Posts' },
+  { value: 'quest', label: 'Quests' },
+  { value: 'request', label: 'Requests' },
+  { value: 'issue', label: 'Issues' },
+];
+
+const PostTypeFilter: React.FC<PostTypeFilterProps> = ({ value, onChange }) => {
+  return (
+    <div className="max-w-xs">
+      <Select
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default PostTypeFilter;
