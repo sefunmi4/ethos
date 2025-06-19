0bf4458def8e7bc1ac56a0519e7dde72000f5885
Sefunmi Ashiru <sefunmiashiru0@gmail.com>
2025-06-19 16:46:15 -0400

    feat: add theme colors

---
+++ ethos-frontend/src/theme.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: '#111827',
+  accent: '#4F46E5',
+  soft: '#F3F4F6',
+  primaryDark: '#f9fafb',
+  softDark: '#1f2937',
+  infoBackground: '#bfdbfe',
+};
+
+export default colors;
