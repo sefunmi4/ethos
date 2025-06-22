fbe1569e645135bab6f506dc8c6031f99578acc2
Sefunmi Ashiru <sefunmiashiru0@gmail.com>
2025-06-22 02:17:28 -0400

    Rearrange PostCard layout

--- ethos-frontend/src/components/post/PostCard.tsx
+++ ethos-frontend/src/components/post/PostCard.tsx
@@ -366,9 +366,9 @@ const PostCard: React.FC<PostCardProps> = ({
             {!isQuestBoardRequest && post.status && post.status !== 'To Do' && (
               <StatusBadge status={post.status} />
             )}
-            <span>{timestamp}</span>
           </div>
         </div>
+        <div className="text-xs text-secondary">{timestamp}</div>
         {titleText && (
           <h3 className="font-semibold text-lg mt-1 cursor-pointer" onClick={() => navigate(ROUTES.POST(post.id))}>
             {titleText}
@@ -437,6 +437,8 @@ const PostCard: React.FC<PostCardProps> = ({
           permalink={`${window.location.origin}${ROUTES.POST(post.id)}`}
         />
       </div>
+
+      <div className="text-xs text-secondary mt-1">{timestamp}</div>
 
       {post.linkedNodeId && post.author?.username && !isQuestBoardRequest && (
         <div className="text-xs text-secondary italic">
