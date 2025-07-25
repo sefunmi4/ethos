87c1639611a5095900f0e4970e561c62e87c2523
Sefunmi Ashiru <sefunmiashiru0@gmail.com>
2025-07-24 22:12:52 -0400

    Fix build by removing extra index signature

--- ethos-frontend/src/hooks/useSocket.ts
+++ ethos-frontend/src/hooks/useSocket.ts
@@ -11,7 +11,6 @@ interface SocketEvents {
   'auth:reset-page-visited': (payload: { token: string }) => void;
   'auth:password-reset-success': (payload: { userId: string }) => void;
   'navigation:404': (payload: { userId: string | null }) => void;
-  [event: string]: (...args: unknown[]) => void;
 }
 
 // ---------------------------
