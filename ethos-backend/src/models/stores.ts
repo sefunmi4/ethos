8da41ce301990dffad567016a87f6809e8b51150
Sefunmi Ashiru <sefunmiashiru0@gmail.com>
2025-08-14 18:23:07 -0400

    feat: replace review store with SQL

--- ethos-backend/src/models/stores.ts
+++ ethos-backend/src/models/stores.ts
@@ -11,6 +11,5 @@ export const questsStore = createDataStore<DBSchema['quests']>('quests.json', []
 export const projectsStore = createDataStore<DBSchema['projects']>('projects.json', []);
 export const usersStore = createDataStore<DBSchema['users']>('users.json', []);
 export const reactionsStore = createDataStore<string[]>('reactions.json', []);
-export const reviewsStore = createDataStore<DBSchema['reviews']>('reviews.json', []);
 export const boardLogsStore = createDataStore<DBSchema['boardLogs']>('boardLogs.json', []);
 export const notificationsStore = createDataStore<DBSchema['notifications']>('notifications.json', []);
